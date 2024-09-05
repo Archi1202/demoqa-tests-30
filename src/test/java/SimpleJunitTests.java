be92ef478b0f8864479132016691a2d71cb5b706
Anuar Zhangeldi <anuar1202@gmail.com>
2024-09-05 18:58:50 +0500

    added jUnit 5 and first tests

--- src/test/java/SimpleJunitTests.java
+++ src/test/java/SimpleJunitTests.java
@@ -1,14 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class SimpleJunitTests {
+
+    private int getResult(){
+        return 3;}
+
+    int result;
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Getting Result from Before Each method!");
+        result = getResult();
+    }
     @Test
-    void SearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    void FirstTest(){
+        System.out.println("###....First Test Started!");
+        Assertions.assertTrue(result > 2,"Statement is not correct");
+    }
+    @Test
+    void SecondTest(){
+        System.out.println("###....Second Test Started!");
+        Assertions.assertTrue(result > 2,"Statement is not correct");
+    }
+
+
+    @Test
+    void ThirdTest(){
+        System.out.println("###....Third Test Started!");
+        Assertions.assertTrue(result > 2,"Statement is not correct");
     }
 }
